d986d3b70d9818a247b7d0bfb156cfa6ffdf832f
GensaGames <GensaGames@gmail.com>
2019-11-04 11:14:16 -0800

    ''omething''

--- apps/rl/SamplePolicyGradient1.py
+++ apps/rl/SamplePolicyGradient1.py
@@ -1,43 +1,50 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 import copy
 
-#Hyperparameters
+# Hyperparameters
 NUM_EPISODES = 4000
 LEARNING_RATE = 0.000025
 GAMMA = 0.99
 
 # Create gym and seed numpy
 env = gym.make('CartPole-v0')
+poly = PolynomialFeatures(1)
 nA = env.action_space.n
-np.random.seed(1)
+# np.random.seed(1)
 
 # Init weight
-w = np.random.rand(4, 2)
+w = np.random.rand(5, 1) - 1
 
 # Keep stats for final print of graph
 episode_rewards = []
 
 
 # Our policy that maps state to action parameterized by w
-def policy(state,w):
-    z = state.dot(w)
-    exp = np.exp(z)
-    return exp/np.sum(exp)
+# noinspection PyShadowingNames
+def policy(state, w):
+    z = np.sum(state.dot(w))
+    return sigmoid(z)
 
 
-# Vectorized softmax Jacobian
-def softmax_grad(softmax):
-    s = softmax.reshape(-1,1)
-    return np.diagflat(s) - np.dot(s, s.T)
+def sigmoid(x):
+    s = 1 / (1 + np.exp(-x))
+    return s
 
 
-# Main loop 
+def sigmoid_grad(x):
+    f = sigmoid(x)
+    return f * (1 - f)
+
+
+# Main loop
 # Make sure you update your weights AFTER each episode
 for e in range(NUM_EPISODES):
 
-    state = env.reset()[None,:]
+    state = env.reset()[None, :]
+    state = poly.fit_transform(state)
 
     grads = []
     rewards = []
@@ -48,23 +55,25 @@ for e in range(NUM_EPISODES):
     while True:
 
         # Uncomment to see your model train in real time (slower)
-        #env.render()
+        # env.render()
 
         # Sample from policy and take action in environment
-        probs = policy(state,w)
-        action = np.random.choice(nA,p=probs[0])
-        next_state,reward,done,_ = env.step(action)
-        next_state = next_state[None,:]
+        probs = policy(state, w)
+        print("Action: " + str(probs))
+        action = int(round(probs))
+        next_state, reward, done, _ = env.step(action)
+        next_state = poly.fit_transform(next_state.reshape(1, 4))
 
         # Compute gradient and save with reward in memory for our weight updates
-        dsoftmax = softmax_grad(probs)[action,:]
-        dlog = dsoftmax / probs[0,action]
-        grad = state.T.dot(dlog[None,:])
+        dsoftmax = sigmoid_grad(probs)
+        dlog = dsoftmax / probs
+        grad = state.T.dot(dlog)
+        grad = grad.reshape(5, 1)
 
         grads.append(grad)
         rewards.append(reward)
 
-        score+=reward
+        score += reward
 
         # Dont forget to update your old state to the new state
         state = next_state
@@ -85,7 +94,7 @@ for e in range(NUM_EPISODES):
 
     # Append for logging and print
     episode_rewards.append(score)
-    print("EP: " + str(e) + " Score: " + str(score) + "         ",end="\r", flush=False)
+    print("EP: " + str(e) + " Score: " + str(score) + "         ", end="\r", flush=False)
 
 plt.plot(np.arange(NUM_EPISODES),
          episode_rewards)
